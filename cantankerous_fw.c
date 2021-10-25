6385929661e4a4b517f1cdc3f6eca9053ae62c4d
Mitchell Johnson <ehntoo@ehntoo.org>
2021-10-25 09:42:42 -0400

    Sane blinky behavior ahoy

--- cantankerous_fw.c
+++ cantankerous_fw.c
@@ -7,49 +7,41 @@
 #include "board/pin_mux.h"
 #include "board.h"
 #include "fsl_power.h"
+#include "cmsis_compiler.h"
+#include "LPC5514.h"
 
-void APPInit() {
+void APPInit(void) {
+    ACT_LED_ON();
+    uint32_t status = SysTick_Config(SystemCoreClock / 1000);
+    if (status != 0) {
+        __BKPT(0);
+    }
     return;
 }
 
-void APPTask() {
+void APPTask(void) {
     return;
+}
+
+void SysTick_Handler(void) {
+    static uint32_t counter = 0;
+    if (counter >= 500) {
+        ACT_LED_TOGGLE();
+        counter = 0;
+    }
+    counter++;
+}
+
+void HardFault_Handler(void) {
+    __BKPT(5);
 }
 
 int main(void) {
     BOARD_InitBootClocks();
     BOARD_InitPins();
-    /* GPIO_PortInit(GPIO, PWR_LED_PORT); */
-    /* GPIO_PortInit(GPIO, ACT_LED_PORT); */
-
 
     /* set BOD VBAT level to 1.65V */
-    /* POWER_SetBodVbatLevel(kPOWER_BodVbatLevel1650mv, kPOWER_BodHystLevel50mv, false); */
-    /* #<{(| attach 12 MHz clock to FLEXCOMM0 (debug console) |)}># */
-    /* CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH); */
-    /* BOARD_InitPins(); */
-    /*  */
-    /* GPIO_PortSet(GPIO, PWR_LED_PORT, 1u << PWR_LED_PIN); */
-    /* GPIO_PortSet(GPIO, ACT_LED_PORT, 1u << ACT_LED_PIN); */
-    /* BOARD_BootClockPLL150M(); */
-    /* BOARD_InitDebugConsole(); */
-    /*  */
-    /* NVIC_ClearPendingIRQ(USB0_IRQn); */
-    /* NVIC_ClearPendingIRQ(USB0_NEEDCLK_IRQn); */
-    /* NVIC_ClearPendingIRQ(USB1_IRQn); */
-    /* NVIC_ClearPendingIRQ(USB1_NEEDCLK_IRQn); */
-    /*  */
-    /* POWER_DisablePD(kPDRUNCFG_PD_USB0_PHY); #<{(|< Turn on USB0 Phy |)}># */
-    /* POWER_DisablePD(kPDRUNCFG_PD_USB1_PHY); #<{(|< Turn on USB1 Phy |)}># */
-    /*  */
-    /* #<{(| reset the IP to make sure it's in reset state. |)}># */
-    /* RESET_PeripheralReset(kUSB0D_RST_SHIFT_RSTn); */
-    /* RESET_PeripheralReset(kUSB0HSL_RST_SHIFT_RSTn); */
-    /* RESET_PeripheralReset(kUSB0HMR_RST_SHIFT_RSTn); */
-    /* RESET_PeripheralReset(kUSB1H_RST_SHIFT_RSTn); */
-    /* RESET_PeripheralReset(kUSB1D_RST_SHIFT_RSTn); */
-    /* RESET_PeripheralReset(kUSB1_RST_SHIFT_RSTn); */
-    /* RESET_PeripheralReset(kUSB1RAM_RST_SHIFT_RSTn); */
+    POWER_SetBodVbatLevel(kPOWER_BodVbatLevel1650mv, kPOWER_BodHystLevel50mv, false);
 
     APPInit();
 
